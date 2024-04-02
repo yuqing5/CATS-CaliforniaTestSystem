96504d90866654c72b3c331129fd8f7be6cb8e69
Yuqing Shen <y5shen@ucsd.edu>
2024-04-02 17:36:09 -0400

    Validation of caiso carbon data

--- myFunc.m
+++ myFunc.m
@@ -1,6 +1,9 @@
-function myFunc(pd, pg)
-    %import cal_grid_dcopf_v2
-    mpc = CaliforniaTestSystem();
-    mpc.bus(:,3) = pd;
-    mpc.gen(:,2) = pg;
+function f = myFunc(real_geni)
+	m = loadcase('CaliforniaTestSystem.m');
+	m.bus(:,3) = (real_geni/sum(m.bus(:,3)))*m.bus(:,3);
+	U = unique(m.gen(:,2));
+	m.gen(:,2) = (real_geni/sum(U))*m.gen(:,2);
+	m.bus(:,4) = (real_geni/sum(m.bus(:,4)))*m.bus(:,4);
+	savecase('CaliforniaTestSystem.m', m);
+	f = sum(m.bus(:,3));
 end
--- run_opf.jl
+++ run_opf.jl
@@ -24,25 +24,23 @@ save_to_JSON = true
 # Specify solver
 #----------------
 # const IPOPT_ENV = Ipopt.Env()
-solver = Ipopt.Optimizer # JuMP.optimizer_with_attributes(() -> Ipopt.Optimizer(), "print_level" => 1)
-
+solver = JuMP.optimizer_with_attributes(() -> Ipopt.Optimizer(), "print_level" => 0) #Ipopt.Optimizer JuMP.optimizer_with_attributes(() -> Ipopt.Optimizer(), "print_level" => 1)
 # Load Data
 # ---------
 # Load the MATPOWER data file
-data = PowerModels.parse_file("MATPOWER/CaliforniaTestSystem.m")
+data = PowerModels.parse_file("CaliforniaTestSystem.m")
 
 ###########
 ## Solve ##
 ###########
 
-solution = PowerModels.solve_opf(data, ACPPowerModel, solver)
-
+solution = PowerModels.solve_opf(data, DCPPowerModel, solver)
 # Save solution dictionary to JSON
 if save_to_JSON == true
     stringdata = JSON.json(solution)
     
     # write the file with the stringdata variable information
-    open("pf_solution_ac.json", "w") do f
+    open("pf_solution.json", "w") do f
         write(f, stringdata)
     end
 end
